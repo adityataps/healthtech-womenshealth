86ea1f9712ff681024e87027524367cf37d7d4f2
Aditya Tapshalkar <aditya.taps@gatech.edu>
2021-03-13 17:57:10 -0500

    added react router, TODO look into url change on card change?

--- healthtech-web/src/App.js
+++ healthtech-web/src/App.js
@@ -1,37 +1,57 @@
 import React from 'react'
 import './App.css';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import {
+    CarouselProvider, Slider, Slide,
+    // ButtonBack,
+    // ButtonNext
+} from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import TestPage from "./pages/testpage";
 
+
+
 function App() {
-  return (
-    <div className={"app-wrapper"}>
-      <CarouselProvider
-      naturalSlideWidth={100}
-      naturalSlideHeight={100}
-      totalSlides={3}
-      // touchEnabled={false}
-      // dragEnabled={false}
-      isIntrinsicHeight={true}
-      >
 
-        <Slider
-        style={{height: "100%", width: "100%", overflow: "hidden"}}
-        >
-          <Slide index={0} className={"card"} style={{backgroundColor: "blue"}}> <TestPage /> </Slide>
-          <Slide index={1} style={{backgroundColor: "red"}}> Slide 1 </Slide>
-          <Slide index={2} style={{backgroundColor: "orange"}}> Slide 2 </Slide>
-        </Slider>
+    return (
+        <div className={"app-wrapper"}>
+            <CarouselProvider
+                naturalSlideWidth={100}
+                naturalSlideHeight={100}
+                totalSlides={3}
+                // touchEnabled={false}
+                // dragEnabled={false}
+                isIntrinsicHeight={true}
+            >
 
-        {/*<ButtonBack> Back </ButtonBack>*/}
-        {/*<ButtonNext> Next </ButtonNext>*/}
-      </CarouselProvider>
+                <Slider
+                    style={{height: "100%", width: "100%", overflow: "hidden"}}
+                >
 
-      {/*<TestPage />*/}
+                    <Slide index={0}
+                           className={"card"} style={{backgroundColor: "blue"}}
+                    >
+                        <TestPage>
+                            {/*{window.history.replaceState(null, "Home", "/")}*/}
+                        </TestPage>
+                    </Slide>
 
-    </div>
-  );
+                    <Slide index={1} style={{backgroundColor: "red"}}>
+                        <TestPage>
+                            {/*{window.history.replaceState(null, "Page2", "/page2")}*/}
+                        </TestPage>
+                    </Slide>
+
+                    <Slide index={2} style={{backgroundColor: "orange"}}>
+                        <TestPage>
+                            {/*{window.history.replaceState(null, "Page3", "/page3")}*/}
+                        </TestPage>
+                    </Slide>
+                </Slider>
+
+            </CarouselProvider>
+
+        </div>
+    );
 }
 
 export default App;
