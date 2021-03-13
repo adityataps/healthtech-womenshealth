4968d87cffc71358a947453ae1c44987e7471a64
Aditya Tapshalkar <aditya.taps@gatech.edu>
2021-03-13 16:46:25 -0500

    fixed carousel card height, added folder structure

--- healthtech-web/src/App.js
+++ healthtech-web/src/App.js
@@ -2,22 +2,34 @@ import React from 'react'
 import './App.css';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import TestPage from "./pages/testpage";
 
 function App() {
   return (
-    <div>
+    <div className={"app-wrapper"}>
       <CarouselProvider
       naturalSlideWidth={100}
       naturalSlideHeight={100}
-      totalSlides={3}>
-        <Slider>
-          <Slide index={0} style={{backgroundColor: "blue"}}> Slide 0 </Slide>
+      totalSlides={3}
+      // touchEnabled={false}
+      // dragEnabled={false}
+      isIntrinsicHeight={true}
+      >
+
+        <Slider
+        style={{height: "100%", width: "100%", overflow: "hidden"}}
+        >
+          <Slide index={0} className={"card"} style={{backgroundColor: "blue"}}> <TestPage /> </Slide>
           <Slide index={1} style={{backgroundColor: "red"}}> Slide 1 </Slide>
           <Slide index={2} style={{backgroundColor: "orange"}}> Slide 2 </Slide>
         </Slider>
-        <ButtonBack> Back </ButtonBack>
-        <ButtonNext> Next </ButtonNext>
+
+        {/*<ButtonBack> Back </ButtonBack>*/}
+        {/*<ButtonNext> Next </ButtonNext>*/}
       </CarouselProvider>
+
+      {/*<TestPage />*/}
+
     </div>
   );
 }
