c156747904bbc2182a11907534dbf7237e3af5cf
Aditya Tapshalkar <aditya.taps@gatech.edu>
2021-03-13 18:45:19 -0500

    added router functionality sorta

--- healthtech-web/src/App.js
+++ healthtech-web/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css';
 import {
-    CarouselProvider, Slider, Slide,
+    CarouselProvider, Slider, Slide, Dot
     // ButtonBack,
     // ButtonNext
 } from 'pure-react-carousel';
+import { BrowserRouter, Route, Link, Redirect } from "react-router-dom";
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import TestPage from "./pages/testpage";
 
@@ -12,46 +13,110 @@ import TestPage from "./pages/testpage";
 
 function App() {
 
+
+
     return (
         <div className={"app-wrapper"}>
-            <CarouselProvider
-                naturalSlideWidth={100}
-                naturalSlideHeight={100}
-                totalSlides={3}
-                // touchEnabled={false}
-                // dragEnabled={false}
-                isIntrinsicHeight={true}
-            >
 
-                <Slider
-                    style={{height: "100%", width: "100%", overflow: "hidden"}}
+            <BrowserRouter>
+
+                <Route exact path={"/"}>
+                    <Redirect to={"/0"} />
+                </Route>
+
+                {console.log(window.location.pathname.substr(window.location.pathname.lastIndexOf('/') + 1))}
+
+                <CarouselProvider
+                    naturalSlideWidth={100}
+                    naturalSlideHeight={100}
+                    totalSlides={3}
+                    // touchEnabled={false}
+                    // dragEnabled={false}
+                    isIntrinsicHeight={true}
+                    currentSlide={window.location.pathname.substr(window.location.pathname.lastIndexOf('/') + 1)}
                 >
 
-                    <Slide index={0}
-                           className={"card"} style={{backgroundColor: "blue"}}
+                    <Slider
+                        style={{height: "100%", width: "100%", overflow: "hidden"}}
                     >
-                        <TestPage>
-                            {/*{window.history.replaceState(null, "Home", "/")}*/}
-                        </TestPage>
-                    </Slide>
 
-                    <Slide index={1} style={{backgroundColor: "red"}}>
-                        <TestPage>
-                            {/*{window.history.replaceState(null, "Page2", "/page2")}*/}
-                        </TestPage>
-                    </Slide>
+                        <Slide index={0}
+                               className={"card"} style={{backgroundColor: "blue"}}
+                        >
+                            {/*<TestPage />*/}
+                            <Route path={"/0"} component={TestPage} />
+                            <NavDots />
 
-                    <Slide index={2} style={{backgroundColor: "orange"}}>
-                        <TestPage>
-                            {/*{window.history.replaceState(null, "Page3", "/page3")}*/}
-                        </TestPage>
-                    </Slide>
-                </Slider>
+                        </Slide>
 
-            </CarouselProvider>
+                        <Slide index={1} style={{backgroundColor: "red"}}>
+                            {/*<TestPage />*/}
+                            <Route path={"/1"} component={TestPage} />
+                            <NavDots />
+
+                        </Slide>
+
+                        <Slide index={2} style={{backgroundColor: "orange"}}>
+                            {/*<TestPage />*/}
+                            <Route path={"/2"} component={TestPage} />
+                            <NavDots />
+
+                        </Slide>
+                    </Slider>
+
+                    {/*<Link to={"/0"}>*/}
+                    {/*    <Dot slide={0}>*/}
+                    {/*        Uno*/}
+                    {/*    </Dot>*/}
+                    {/*</Link>*/}
+
+                    {/*<Link to={"/1"}>*/}
+                    {/*    <Dot slide={1}>*/}
+                    {/*        Dos*/}
+                    {/*    </Dot>*/}
+                    {/*</Link>*/}
+
+                    {/*<Link to={"/2"}>*/}
+                    {/*    <Dot slide={2}>*/}
+                    {/*        Tres*/}
+                    {/*    </Dot>*/}
+                    {/*</Link>*/}
+
+                    {/*<Route path={"/"}>*/}
+                    {/*    <Redirect to={"/0"} />*/}
+                    {/*</Route>*/}
+
+
+                </CarouselProvider>
+
+            </BrowserRouter>
 
         </div>
     );
+}
+
+function NavDots() {
+    return (
+        <div>
+            <Link to={"/0"}>
+                <Dot slide={0}>
+                    Uno
+                </Dot>
+            </Link>
+
+            <Link to={"/1"}>
+                <Dot slide={1}>
+                    Dos
+                </Dot>
+            </Link>
+
+            <Link to={"/2"}>
+                <Dot slide={2}>
+                    Tres
+                </Dot>
+            </Link>
+        </div>
+    )
 }
 
 export default App;
